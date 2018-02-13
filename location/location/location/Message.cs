a39dd71e2831aa59dcb4eb7daad21b82425226dd
James Gordon <james.gordon1628@gmail.com>
2018-02-13 22:25:52 +0000

    Edited Message to understand what the difference cases are

--- location/location/location/Message.cs
+++ location/location/location/Message.cs
@@ -6,29 +6,28 @@ using System.Threading.Tasks;
 
 namespace location
 {
-    enum MessageProtocol {HTTP9, HTTP1,  }
+    enum MessageProtocol {HTTP9, HTTP1, HTTP11, WhoIs  }
     enum MessageType { lookup, update }
     class Message
     {
-
+        private MessageType m_Type;
+        private MessageProtocol m_Protocol;
         public Message(string[] args) {
             if (args.Contains("HTTP/1.0"))
             {
-
+                m_Protocol = MessageProtocol.HTTP1;
             }
             else if (args.Contains("HTTP/1.1"))
             {
-
-            }
-            else if (args.Length == 2 && args.Contains("GET")) {
-
+                m_Protocol = MessageProtocol.HTTP11;
             }
             switch (args.Length) {
                 case 1:
-
+                    //Not a HTTP 0.9 request just a whois request
+                    m_Protocol = MessageProtocol.WhoIs;
                     break;
                 case 2:
-
+                    //could be a HTTP 0.9 request as a get or a who is update 
                     break;
                 default:
                     break;
