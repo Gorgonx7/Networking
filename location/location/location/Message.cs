193126ad63d9083bf66f41490f6cc094075b76f4
James Gordon <james.gordon1628@gmail.com>
2018-02-14 15:45:47 +0000

    Finished the detection algorithm that decided what type of message and what protocol to use for that Message when it is sent

--- location/location/location/Message.cs
+++ location/location/location/Message.cs
@@ -16,21 +16,63 @@ namespace location
             if (args.Contains("HTTP/1.0"))
             {
                 m_Protocol = MessageProtocol.HTTP1;
+                if (args[0] == "POST")
+                {
+                    m_Type = MessageType.update;
+                }
+                else if (args[0] == "GET")
+                {
+                    m_Type = MessageType.lookup;
+                }
             }
             else if (args.Contains("HTTP/1.1"))
             {
                 m_Protocol = MessageProtocol.HTTP11;
+                if (args[0] == "POST")
+                {
+                    m_Type = MessageType.update;
+                }
+                else if (args[0] == "GET")
+                {
+                    m_Type = MessageType.lookup;
+                }
             }
-            switch (args.Length) {
-                case 1:
-                    //Not a HTTP 0.9 request just a whois request
-                    m_Protocol = MessageProtocol.WhoIs;
-                    break;
-                case 2:
-                    //could be a HTTP 0.9 request as a get or a who is update 
-                    break;
-                default:
-                    break;
+            else
+            {
+                switch (args.Length)
+                {
+                    case 1:
+                        //Not a HTTP 0.9 request just a whois request
+                        m_Protocol = MessageProtocol.WhoIs;
+                        m_Type = MessageType.lookup;
+                        break;
+                    case 2:
+                        //could be a HTTP 0.9 request as a get or a who is update
+                        if (args[1][0] == '/')
+                        {
+                            m_Protocol = MessageProtocol.HTTP9;
+                            if (args[0] == "GET")
+                            {
+                                m_Type = MessageType.lookup;
+                            }
+                            else if (args[0] == "PUT")
+                            {
+                                m_Type = MessageType.update;
+                            }
+                            else
+                            {
+                                throw new Exception("Protocol not implimented");
+                            }
+                        }
+                        else
+                        {
+                            m_Protocol = MessageProtocol.WhoIs;
+                            m_Type = MessageType.update;
+                        }
+                        break;
+                    default:
+                        break;
+                }
             }
         }
 
