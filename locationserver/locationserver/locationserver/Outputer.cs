5e4915bdf6c6e59d8b4317135894e78c11070ad9
James Gordon <james.gordon1628@gmail.com>
2018-03-04 15:52:11 +0000

    Finished the Protocol output for each type

--- locationserver/locationserver/locationserver/Outputer.cs
+++ locationserver/locationserver/locationserver/Outputer.cs
@@ -15,6 +15,7 @@ namespace locationserver
        
 
         public static void Update(StreamWriter pWriter, Protocol pProtocol) {
+            string Output = "";
             switch (pProtocol)
             {
                 case Protocol.WhoIs:
@@ -23,34 +24,112 @@ namespace locationserver
                     pWriter.Close();
                     break;
                 case Protocol.HTTP1:
-                    throw new NotImplementedException();
+                    /*
+                     * HTTP/1.0<space>200<space>OK<CR><LF>
+                       Content-Type:<space>text/plain<CR><LF>
+                       <CR><LF>*/
+                    Output = "HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\n";
+                    pWriter.WriteLine(Output);
                     break;
                 case Protocol.HTTP11:
-                    throw new NotImplementedException();
+                    /*  HTTP/1.1<space>200<space>OK<CR><LF>
+                        Content-Type:<space>text/plain<CR><LF>
+                        <optional header lines><CR><LF>*/
+
+                    Output = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n";
+                    pWriter.WriteLine(Output);
                     break;
                 case Protocol.HTTP9:
-                    throw new NotImplementedException();
+                    /*
+                     *  HTTP/0.9<space>200<space>OK<CR><LF>
+                        Content-Type:<space>text/plain<CR><LF>
+                        <CR><LF>
+                        */
+                     Output = "HTTP/0.9 200 OK\r\nContent-Type: text/plain\r\n";
+                    pWriter.WriteLine(Output);
                     break;
             }
+            Console.WriteLine(">> Replying to client with: ");
+            Console.Write(Output);
+            pWriter.Flush();
+            pWriter.Close();
         }
         public static void Locate(StreamWriter pWriter, Protocol pProtocol, string pName) {
+            string output = "";
             switch (pProtocol)
             {
                 case Protocol.WhoIs:
                     pWriter.WriteLine(Program.m_Manager.GetLocation(pName));
-                    pWriter.Flush();
-                    pWriter.Close();
+                   
                     break;
                 case Protocol.HTTP1:
-                    throw new NotImplementedException();
+                    if (Program.m_Manager.GetLocation(pName) == "ERROR: no entries found")
+                    {
+                        /*
+                         *  HTTP/1.0<space>404<space>Not<space>Found<CR><LF>
+                            Content-Type:<space>text/plain<CR><LF>
+                            <CR><LF>*/
+                        output = "HTTP/1.0 404 Not Found\r\nContent-Type: text/plain\r\n";
+                        pWriter.WriteLine(output);
+                    }
+                    else
+                    {
+                        /*
+                         *  HTTP/1.0<space>200<space>OK<CR><LF>
+                            Content-Type:<space>text/plain<CR><LF>
+                            <CR><LF>
+                            <location><CR><LF>*/
+                        output = "HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\n\r\n" + Program.m_Manager.GetLocation(pName);
+                        pWriter.WriteLine(output);
+                    }
                     break;
                 case Protocol.HTTP11:
-                    throw new NotImplementedException();
+                    if (Program.m_Manager.GetLocation(pName) == "ERROR: no entries found")
+                    {
+                        /*
+                        HTTP/1.1<space>404<space>Not<space>Found<CR><LF>
+                        Content-Type:<space>text/plain<CR><LF>
+                        <optional header lines><CR><LF>    
+                     */
+                        output = "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\n";
+                        pWriter.WriteLine(output);
+                    }
+                    else
+                    {
+                        /*  HTTP/1.1<space>200<space>OK<CR><LF>
+                            Content-Type:<space>text/plain<CR><LF>
+                            <optional header lines><CR><LF>
+                            <location><CR><LF>*/
+                        output = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n" + Program.m_Manager.GetLocation(pName);
+                        pWriter.WriteLine(output);
+                    }
                     break;
                 case Protocol.HTTP9:
-                    throw new NotImplementedException();
+                    if (Program.m_Manager.GetLocation(pName) == "ERROR: no entries found")
+                    {
+                        /*
+                         *  HTTP/0.9<space>404<space>Not<space>Found<CR><LF>
+                            Content-Type:<space>text/plain<CR><LF>
+                            <CR><LF>*/
+                        output = "HTTP/0.9 404 Not Found\r\nContent-Type: text/plain\r\n";
+                        pWriter.WriteLine(output);
+                    }
+                    else
+                    {
+                        /*
+                         *  HTTP/0.9<space>200<space>OK<CR><LF>
+                            Content-Type:<space>text/plain<CR><LF>
+                            <CR><LF>
+                            <location><CR><LF>*/
+                        output = "HTTP/0.9 200 OK\r\nContent-Type: text/plain\r\n\r\n" + Program.m_Manager.GetLocation(pName);
+                        pWriter.WriteLine();
+                    }
                     break;
             }
+            Console.WriteLine(">> Replying to client with: ");
+            Console.Write(output);
+            pWriter.Flush();
+            pWriter.Close();
         }
 
     }
