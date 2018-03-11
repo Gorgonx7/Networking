6766f3313f9bee53893d2181a537f2dda6ebfb49
James Gordon <james.gordon1628@gmail.com>
2018-03-11 13:57:48 +0000

    written a algorithm to write data in in bytes

--- location/location/location/ReplyHandler.cs
+++ location/location/location/ReplyHandler.cs
@@ -10,11 +10,12 @@ namespace location
     {
         public ReplyHandler(StreamReader sr, Message pMessage)
         {
+            
             if (pMessage.GetProtocol() == MessageProtocol.WhoIs)
             {
                 try
                 {
-                    string holder = sr.ReadToEnd();
+                   string holder = Readdata(sr);
                     if (holder == "OK\r\n" && pMessage.getType() == MessageType.update)
                     {
                         Console.WriteLine(pMessage.GetName() + " location changed to be " + pMessage.GetLocation());
@@ -58,6 +59,43 @@ namespace location
             }
 
 
+
+        }
+        private string Readdata(StreamReader sr) {
+            Stream stream = sr.BaseStream;
+            byte[] buffer = new byte[25];
+            long read = 0;
+
+            int chunk;
+            while ((chunk = stream.Read(buffer, (int)read, buffer.Length - (int)read)) > 0)
+            {
+                read += chunk;
+
+                // If we've reached the end of our buffer, check to see if there's
+                // any more information
+                if (read == buffer.Length)
+                {
+                    int nextByte = stream.ReadByte();
+
+                    // End of stream? If so, we're done
+                    if (nextByte == -1)
+                    {
+                        return System.Text.Encoding.ASCII.GetString(buffer);
+                    }
+
+                    // Nope. Resize the buffer, put in the byte we've just
+                    // read, and continue
+                    byte[] newBuffer = new byte[buffer.Length * 2];
+                    Array.Copy(buffer, newBuffer, buffer.Length);
+                    newBuffer[read] = (byte)nextByte;
+                    buffer = newBuffer;
+                    read++;
+                }
+            }
+            // Buffer is now too big. Shrink it.
+            byte[] ret = new byte[read];
+            Array.Copy(buffer, ret, read);
+            return System.Text.Encoding.ASCII.GetString(buffer);
 
         }
     }
