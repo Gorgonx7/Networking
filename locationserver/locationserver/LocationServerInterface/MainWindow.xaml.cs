ed88257bdcae1591ccea76e8712a0b7b8f87be12
James Gordon <james.gordon1628@gmail.com>
2018-03-17 19:01:14 +0000

    implemented some Tidy up source code for the log window

--- locationserver/locationserver/LocationServerInterface/MainWindow.xaml.cs
+++ locationserver/locationserver/LocationServerInterface/MainWindow.xaml.cs
@@ -20,9 +20,46 @@ namespace LocationServerInterface
     /// </summary>
     public partial class MainWindow : Window
     {
+        Log LogWindow = new Log();
+        bool LogIsOpen = false;
         public MainWindow()
         {
+            
             InitializeComponent();
+        }
+        private void Start_Click(Object sender, RoutedEventArgs e) {
+            if ((string)startstopbutton.Content == "Start")
+            {
+                // start the server
+                StatusImage_GreenTick.Visibility = Visibility.Visible;
+                StatusImage_RedCross.Visibility = Visibility.Hidden;
+                startstopbutton.Content = "Stop";
+            }
+            else
+            {
+                // stop the server
+                StatusImage_GreenTick.Visibility = Visibility.Hidden;
+                StatusImage_RedCross.Visibility = Visibility.Visible;
+                startstopbutton.Content = "Start";
+            }
+        }
+        private void Openlog_Click(object sender, RoutedEventArgs e)
+        {
+            if (!LogIsOpen)
+            {
+                LogWindow.Activate();
+                LogWindow.Show();
+                LogIsOpen = true;
+            }
+            else {
+                LogWindow.Visibility = Visibility.Hidden;
+                LogIsOpen = false;
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            LogWindow.Close();
         }
     }
 }
